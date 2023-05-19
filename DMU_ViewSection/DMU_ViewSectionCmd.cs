a6659474bc4d5aaf605231c3819c1a5e51ef62d1
TonKha12345 <trantonkha1999@gmail.com>
2023-05-19 15:02:07 +0700

    Code C# Finish

---
+++ DMU_ViewSection/DMU_ViewSectionCmd.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace DMU_ViewSection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class DMU_ViewSectionCmd : IExternalCommand
+    {
+        Document m_document;
+        UIDocument m_documentUI;
+
+        //application's private data
+        private DMU_ViewSectionUpdater m_viewSectionUpdater = null;
+        private AddInId m_thisAppId;
+        private List<ElementId> idsToWatch = new List<ElementId>();
+        private ElementId m_oldSectionId = ElementId.InvalidElementId;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            m_documentUI = commandData.Application.ActiveUIDocument;
+            m_document = m_documentUI.Document;
+            m_thisAppId = commandData.Application.ActiveAddInId;
+
+            try
+            {
+                //Creating and registering the updater for the document
+                if (m_viewSectionUpdater == null)
+                {
+                    using (Transaction trans = new Transaction(m_document, " Register"))
+                    {
+                        trans.Start();
+                        m_viewSectionUpdater = new DMU_ViewSectionUpdater(m_thisAppId);
+                        m_viewSectionUpdater.Register(m_document);
+                        trans.Commit();
+                    }
+                }
+
+                MessageBox.Show("Select a section view, then select a window", "Information");
+
+                Element sectionElement = null; //Section Element pick object
+                Element window = null;
+                Element section = null; //Section Element in view
+                try
+                {
+                    //Get section Element
+                    Reference refSection = m_documentUI.Selection.PickObject(ObjectType.Element, "Select section View");
+                    if (refSection != null)
+                    {
+                        Element sectionElem = m_document.GetElement(refSection);
+                        if(sectionElem != null)
+                        {
+                            sectionElement = sectionElem;
+                        }
+                    }
+
+                    //Get window
+                    Reference refWindow = m_documentUI.Selection.PickObject(ObjectType.Element, "Select window");
+                    if(refWindow != null)
+                    {
+                        Element win = m_document.GetElement(refWindow);
+                        if(win != null)
+                        {
+                            window = win;
+                        }
+                    }
+                }
+                catch(OperationCanceledException)
+                {
+                    MessageBox.Show("The selection to be fauld", "Information");
+                    return Result.Cancelled;
+                }
+
+                if(window == null || sectionElement == null)
+                {
+                    MessageBox.Show("Window is null", "Error");
+                    return Result.Cancelled;
+                }
+
+                //Find the real ViewSection for the selected section element
+                List<Element> sectionViews = new FilteredElementCollector(m_document).OfCategory(BuiltInCategory.OST_Views).Where(e => e.Name.Equals(sectionElement.Name)).ToList();    
+                if(sectionViews.Count == 0)
+                {
+                    MessageBox.Show("Does not view Element in Views", "Error");
+                    return Result.Failed;
+                }
+                section = sectionViews.FirstOrDefault();
+
+                //The section view associcated to the window, and add trigger for it
+                if(!idsToWatch.Contains(window.Id) || m_oldSectionId != section.Id)
+                {
+                    idsToWatch.Clear();
+                    idsToWatch.Add(window.Id);
+                    m_oldSectionId = section.Id;
+                    UpdaterRegistry.RemoveAllTriggers(m_viewSectionUpdater.GetUpdaterId());
+                    m_viewSectionUpdater.AddTriggerForUpdate(m_document, idsToWatch, section.Id, sectionElement);
+                    MessageBox.Show("The ViewSection id: " + section.Id.ToString() + "has been associate to the window id: " + window.Id.ToString() + "\n You can try to move or rotate window to see update ", "Information");
+                }
+                else
+                {
+                    MessageBox.Show("The model has been already associated to the ViewSection", "Information");
+                }
+
+                m_document.DocumentClosing += UnregisterSectionUpdaterOnClose;
+                return Result.Succeeded;
+            }
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+
+        private void UnregisterSectionUpdaterOnClose(object source, DocumentClosingEventArgs args)
+        {
+            idsToWatch.Clear();
+            m_oldSectionId = ElementId.InvalidElementId;
+            if(m_viewSectionUpdater != null)
+            {
+                UpdaterRegistry.UnregisterUpdater(m_viewSectionUpdater.GetUpdaterId());
+                m_viewSectionUpdater = null;
+            }
+        }
+    }
+}
---
+++ DMU_ViewSection/DMU_ViewSectionUpdater.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace DMU_ViewSection
+{
+    public class DMU_ViewSectionUpdater: IUpdater
+    {
+        private UpdaterId m_updaterId = null;
+        private ElementId m_windowId = null;
+        private ElementId m_sectionId = null;       // The real ViewSection that contains the origin and ViewDirection
+        private Element m_sectionElement = null;    // The view section Element to move and rotate
+
+        public DMU_ViewSectionUpdater(AddInId addinId)
+        {
+            m_updaterId = new UpdaterId(addinId, new Guid("066400B2-BB97-44CF-9853-C8FB4091B31E"));
+        }
+
+        internal void Register(Document doc)
+        {
+            if(!UpdaterRegistry.IsUpdaterRegistered(m_updaterId))
+            {
+                UpdaterRegistry.RegisterUpdater(this, doc);
+            }
+        }
+
+        internal void AddTriggerForUpdate(Document doc, List<ElementId> idsToWatch, ElementId sectionId, Element sectionElement)
+        {
+            if (idsToWatch.Count == 0) return;
+            m_windowId = idsToWatch.FirstOrDefault();
+            m_sectionId = sectionId;
+            m_sectionElement = sectionElement;
+            UpdaterRegistry.AddTrigger(m_updaterId, doc, idsToWatch, Element.GetChangeTypeGeometry());
+        }
+
+        internal void RejustSectionView(Document doc, Element elem, ViewSection section)
+        {
+            //Get position and rotation of FamilyInstance
+            XYZ position = XYZ.Zero;
+            XYZ fOrientation = XYZ.Zero;
+            if(elem is FamilyInstance)
+            {
+                FamilyInstance familyInstance = elem as FamilyInstance;
+                if(familyInstance.Location != null && familyInstance.Location is LocationPoint)
+                {
+                    LocationPoint locationPoint = familyInstance.Location as LocationPoint;
+                    position = locationPoint.Point;
+                }
+                fOrientation = familyInstance.FacingOrientation;
+            }
+
+            //Get position and Direction of view section
+            XYZ sOrigin = section.Origin;
+            XYZ sDirection = section.ViewDirection;
+
+            XYZ fRectOrientation = fOrientation.CrossProduct(XYZ.BasisZ);
+
+            //Rotate the section Element
+            double angle = fOrientation.AngleTo(sDirection);
+
+            //Need to adjust the rotation angle based on the direction of rotation
+            XYZ cross = fRectOrientation.CrossProduct(sDirection).Normalize();
+
+            double sign = 1.0;
+            if (!cross.IsAlmostEqualTo(XYZ.BasisZ))
+            {
+                sign = -1.0;
+            }
+
+            double rotateAngle = 0.0;
+            if(Math.Abs(angle) > 0 && Math.Abs(angle) <= Math.PI / 2)
+            {
+                if(angle < 0)
+                {
+                    rotateAngle = Math.PI / 2.0 + angle;
+                }
+                else
+                {
+                    rotateAngle = Math.PI / 2.0 - angle;
+                }
+            }
+            else if(Math.Abs(angle) > Math.PI / 2)
+            {
+                if(angle < 0)
+                {
+                    rotateAngle = angle + Math.PI / 2.0;
+                }
+                else
+                {
+                    rotateAngle = angle - Math.PI / 2.0;
+                }
+            }
+            rotateAngle *= sign; 
+
+            if(Math.Abs(rotateAngle) > 0)
+            {
+                Line axis = Line.CreateBound(sOrigin, sOrigin + XYZ.BasisZ);
+                ElementTransformUtils.RotateElement(doc, m_sectionElement.Id, axis, rotateAngle);
+            }
+
+            //Regenerate the document
+            doc.Regenerate();
+
+            //Move the section Element
+            double dotF = position.DotProduct(fRectOrientation);
+            double dotS = sOrigin.DotProduct(fRectOrientation);
+            double moveDot = dotF - dotS;
+            XYZ sNewDirection = section.ViewDirection;
+            double correction = fRectOrientation.DotProduct(sNewDirection);
+            XYZ translationVec = sNewDirection * correction * moveDot;
+
+            if (!translationVec.IsZeroLength())
+            {
+                ElementTransformUtils.MoveElement(doc,m_sectionElement.Id, translationVec);
+            }
+
+        }
+
+        public void Execute(UpdaterData data)
+        {
+            try
+            {
+                Document doc = data.GetDocument();
+                //through modified elements to find which we want the section to follow
+                foreach(ElementId id in data.GetModifiedElementIds())
+                {
+                    if(id == m_windowId)
+                    {
+                        FamilyInstance window = doc.GetElement(id) as FamilyInstance;
+                        ViewSection section = doc.GetElement(m_sectionId) as ViewSection;
+                        RejustSectionView(doc, window, section);
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                MessageBox.Show(e.ToString(), "Error");
+            }
+            return;
+        }
+
+        public string GetAdditionalInformation()
+        {
+            return "Contact via email trantonkha1999@gmail.com";
+        }
+
+        public ChangePriority GetChangePriority()
+        {
+            return ChangePriority.Views;
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            return m_updaterId;
+        }
+
+        public string GetUpdaterName()
+        {
+            return "Section Update";
+        }
+
+        
+    }
+}
